df1548d3da4a21db6d3beb0c4aca7316d6e3f1f7
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-05-10 15:43:12 -0500

    Each thread now initializes a CURL object once

--- main.c
+++ main.c
@@ -1,36 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h> 
+#include <curl/curl.h>
 
 #include "queue.h"
 #include "bst.h"
 
 #include "url_conversion.h"
-#include "mycurl.h"
 #include "get_row.h"
 
 #define ROW_COUNT (getBSTCount()-getQueueCount())
+#define THREAD_NUM 32
 
 struct Queue* queue;
 struct BST_Node* bst_root;
 
 static inline void mutex_write(struct Row* row);
 
+struct HTML {
+    size_t size;
+    char* data;
+};
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, struct HTML *html)
+{
+    size_t index = html->size;
+    size_t n = (size * nmemb);
+    char* tmp;
+
+    html->size += (size * nmemb);
+    tmp = realloc(html->data, html->size + 1); /* +1 for '\0' */
+
+    if(tmp) {
+        html->data = tmp;
+    } else {
+        if(html->data) {
+            free(html->data);
+        }
+        fprintf(stderr, "Failed to allocate memory.\n");
+        return 0;
+    }
+
+    memcpy((html->data + index), ptr, n);
+    html->data[html->size] = '\0';
+
+    return size * nmemb;
+}
+
+int max_size = 0;
+
 void* runner(void* args)
 {
 	struct Row row;
-	char* html_data;
+	struct HTML html;
+	char full_url[43] = "https://www.youtube.com/watch?v=";
+
+	CURL *curl;
+	curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &html);
 
 	while (row.id = pop(queue)) {
+		html.data = malloc(4096);
+		html.data[0] = '\0';
+		html.size = 0;
+
 		char ascii_id[11];
-		html_data = download_webpage(row.id);
-		get_row(html_data, &row);
+
+		lltourl(row.id, &full_url[32]);
+		curl_easy_setopt(curl, CURLOPT_URL, full_url);
+
+		CURLcode res = curl_easy_perform(curl);
+		if (res != CURLE_OK)
+			fprintf(stderr, "ERROR: curl_easy_perform failed\n"),
+			exit(1);
+
+		if (max_size < html.size) {
+			max_size = html.size;
+			fprintf(stderr, "new max size: %s, %d\n", full_url, max_size);
+		}
+
+		get_row(html.data, &row);
 		if (row.recommendations[REC_COUNT-1] == 0) {
 			push(queue, row.id);
-			free(html_data);
+			free(html.data);
 			continue;
 		}
 		for (int32_t i = 0; i < REC_COUNT; i++) {
@@ -38,14 +95,13 @@ void* runner(void* args)
 				push(queue, row.recommendations[i]);
 		}
 		mutex_write(&row);
-		free(html_data);
+		free(html.data);
 		if (ROW_COUNT % 100 == 0)
-			printf("rows = %lu, bst = %lu, queue = %lu\n", ROW_COUNT, getBSTCount(), getQueueCount());
+			fprintf(stderr, "rows = %lu, bst = %lu, queue = %lu\n", ROW_COUNT, getBSTCount(), getQueueCount());
 	}
+	curl_easy_cleanup(curl);
 	pthread_exit(0);
 }
-
-#define THREAD_NUM 32
 
 int main()
 {
@@ -58,7 +114,7 @@ int main()
 		int32_t fd;
 		int32_t size;
 		if ((fd = open("youtube.bin", O_RDONLY, 0755)) != -1) {
-			printf("Loading 'youtube.bin'...\n");
+			fprintf(stderr, "Loading 'youtube.bin'...\n");
 			// load primary keys
 			while (size = read(fd, &buffer, sizeof(struct Row))) {
 				if (size == -1) {
@@ -78,9 +134,9 @@ int main()
 						push(queue, buffer.recommendations[i]);
 			}
 			close(fd);
-			printf("rows = %lu, bst = %lu, stack = %lu\n", ROW_COUNT, getBSTCount(), getQueueCount());
+			fprintf(stderr, "rows = %lu, bst = %lu, stack = %lu\n", ROW_COUNT, getBSTCount(), getQueueCount());
 		} else {
-			printf("No file found: using default id\n");
+			fprintf(stderr, "No file found: using default id\n");
 			BST_insert(&bst_root, urltoll(default_id));
 			push(queue, urltoll(default_id));
 		}
@@ -94,7 +150,7 @@ int main()
 			pthread_create(&tids[i], &attr, runner, NULL);
 			while (getQueueCount() < 10)
 			{ /* wait for first thread to push its first set of IDs */ }
-			printf("thread %d in\n", i+1);
+			fprintf(stderr, "thread %d in\n", i+1);
 		}
 
 		for (int32_t i = 0; i < THREAD_NUM; i++) {
