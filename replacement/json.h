6c30f6bf1c05c54f02e5d92b17edfaf27b5eac54
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-07-25 18:36:08 -0500

    I can now download a webpage using https with openssl

--- replacement/json.h
+++ replacement/json.h
@@ -2,6 +2,20 @@
 
 #include <string.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
 extern FILE* yyin;
 extern int yylineno;
 
--- replacement/main.c
+++ replacement/main.c
@@ -5,12 +5,7 @@
 
 #include "json.h"
 
-extern struct Video_Row vRow;
-extern struct Channel_Row cRow;
-
 int main(int argc, char *argv[])
 {
-	yyin = (argc == 2) ? fopen(argv[1],"r") : fopen("yt.json", "r");
-
 	yylex();
 }
