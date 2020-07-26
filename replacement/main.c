9f9107de8eb611369c9c39a2ad3c64b18c73f081
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-07-26 01:30:24 -0500

    Program can now crawl to different videos. Does not store anything to drive yet

--- replacement/main.c
+++ replacement/main.c
@@ -10,12 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	//char start_id[] = "3nrLc_JOF7k"; // ordinary
-	char start_id[] = "x71MDrC400A"; // age-restricted
+	char start_id[] = "3nrLc_JOF7k"; // ordinary
+	//char start_id[] = "x71MDrC400A"; // age-restricted
 	int64_t start_id_int = decode64(start_id);
 
 	push(start_id_int);
 	video_insert(start_id_int);
 
-	yylex();
+	while (yylex());
 }
