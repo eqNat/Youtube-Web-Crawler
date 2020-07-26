19c1c3c4ece59cf9812c0809848f1e6e666ac359
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-07-26 00:36:43 -0500

    flex now recognizes age-restricted videos, which don't show recommendations when logged out.

--- replacement/main.c
+++ replacement/main.c
@@ -10,8 +10,10 @@
 
 int main(int argc, char *argv[])
 {
-	char start_id[] = "3nrLc_JOF7k";
+	//char start_id[] = "3nrLc_JOF7k"; // ordinary
+	char start_id[] = "x71MDrC400A"; // age-restricted
 	int64_t start_id_int = decode64(start_id);
+
 	push(start_id_int);
 	video_insert(start_id_int);
 
