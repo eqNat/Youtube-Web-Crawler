cad7c042a4b44746ce053e191245f09694d05822
Nuclear-Catapult <nuclear.catapult@gmail.com>
2020-08-02 10:24:19 -0500

    Wrote a comment about my ignorance of function attributes

--- panic.h
+++ panic.h
@@ -6,5 +6,7 @@
 
 #define PANIC(...) nc_panic(__FILE__, __LINE__, __VA_ARGS__)
 
+// I don't know if it makes a difference to use the 'format' attribute if
+// printf will validate the string anyways
 __attribute__((noreturn, cold, format (printf, 3, 4)))
 void nc_panic(const char* file_name, int line_number, const char* format, ...);
